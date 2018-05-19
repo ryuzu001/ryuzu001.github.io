22ca4f9f60756a5d5124d197890a49a78d8bfa57
Ryan Yuzuki <ryuzu001@ucr.edu>
2018-05-19 00:39:37 -0700

    emoji

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,40 @@
-// alert("Welcome to my website -Ryan");
+var x = Math.floor(Math.random() * 10); // 0 to 9
+var emoji;
+
+switch(x){
+  case 0:
+    emoji="❤️";
+    break;
+  case 1:
+    emoji="🍕";
+    break;
+  case 2:
+    emoji="🍛";
+    break;
+  case 3:
+    emoji="🍙";
+    break;
+  case 4:
+    emoji="☕";
+    break;
+  case 5:
+    emoji="💖";
+    break;
+  case 6:
+    emoji="🍣";
+    break;
+  case 7:
+    emoji="🍩";
+    break;
+  case 8:
+    emoji="🍦";
+    break;
+  case 9:
+    emoji="🍱";
+    break;
+  default:
+    emoji="❤️";
+    break;
+}
+
+document.getElementById("emoji").innerHTML = emoji;
