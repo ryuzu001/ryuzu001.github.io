6b2b5046134e9ff5a3f8f160c59b85f235a8ae43
ryuzu001 <ryuzu001@ucr.edu>
2026-05-19 00:43:44 -0700

    messy progress

---
+++ scripts/map.js
@@ -0,0 +1,17 @@
+
+const wrapperNode = document.querySelector('.embla')
+const viewportNode = wrapperNode.querySelector('.embla__viewport')
+const emblaApi = EmblaCarousel(viewportNode, { loop: false })
+
+const prevButtonNode = wrapperNode.querySelector('.embla__prev')
+const nextButtonNode = wrapperNode.querySelector('.embla__next')
+
+prevButtonNode.addEventListener('click', () => emblaApi.scrollPrev(), false)
+nextButtonNode.addEventListener('click', () => emblaApi.scrollNext(), false)
+
+const toggleButtonsDisabled = (emblaApi) => {  const setButtonState = (button, enabled) => {    button.toggleAttribute('disabled', !enabled)  }  
+setButtonState(prevButtonNode, emblaApi.canScrollPrev())  
+setButtonState(nextButtonNode, emblaApi.canScrollNext())}
+toggleButtonsDisabled(emblaApi)
+emblaApi.on('select', toggleButtonsDisabled)
+emblaApi.on('reInit', toggleButtonsDisabled)
