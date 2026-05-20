20de2c5da17da8f314b6dff17c6692fab9694da6
ryuzu001 <ryuzu001@ucr.edu>
2026-05-20 00:09:29 -0700

    cleanup

--- scripts/map.js
+++ scripts/map.js
@@ -3,15 +3,15 @@ const wrapperNode = document.querySelector('.embla')
 const viewportNode = wrapperNode.querySelector('.embla__viewport')
 const emblaApi = EmblaCarousel(viewportNode, { loop: false })
 
-const prevButtonNode = wrapperNode.querySelector('.embla__prev')
-const nextButtonNode = wrapperNode.querySelector('.embla__next')
+// const prevButtonNode = wrapperNode.querySelector('.embla__prev')
+// const nextButtonNode = wrapperNode.querySelector('.embla__next')
 
-prevButtonNode.addEventListener('click', () => emblaApi.scrollPrev(), false)
-nextButtonNode.addEventListener('click', () => emblaApi.scrollNext(), false)
+// prevButtonNode.addEventListener('click', () => emblaApi.scrollPrev(), false)
+// nextButtonNode.addEventListener('click', () => emblaApi.scrollNext(), false)
 
-const toggleButtonsDisabled = (emblaApi) => {  const setButtonState = (button, enabled) => {    button.toggleAttribute('disabled', !enabled)  }  
-setButtonState(prevButtonNode, emblaApi.canScrollPrev())  
-setButtonState(nextButtonNode, emblaApi.canScrollNext())}
-toggleButtonsDisabled(emblaApi)
-emblaApi.on('select', toggleButtonsDisabled)
-emblaApi.on('reInit', toggleButtonsDisabled)
+// const toggleButtonsDisabled = (emblaApi) => {  const setButtonState = (button, enabled) => {    button.toggleAttribute('disabled', !enabled)  }  
+// setButtonState(prevButtonNode, emblaApi.canScrollPrev())  
+// setButtonState(nextButtonNode, emblaApi.canScrollNext())}
+// toggleButtonsDisabled(emblaApi)
+// emblaApi.on('select', toggleButtonsDisabled)
+// emblaApi.on('reInit', toggleButtonsDisabled)
