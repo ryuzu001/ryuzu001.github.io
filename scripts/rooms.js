beecea0abef360ff395f6bf5cfbf890d5aa553c8
ryuzu001 <ryuzu001@ucr.edu>
2026-05-28 01:22:00 -0700

    optimization

---
+++ scripts/rooms.js
@@ -0,0 +1,153 @@
+const rooms = [
+      {
+        "lat": 34.03929054041106, 
+        "lng": -118.32032562275671,
+        "size": 0.01,
+        "color": "red",
+        "name": "Hatch Escapes",
+        "rooms": [ 
+          { "photo": "hatch1", "name": "Lab Rat" },
+          { "photo": "hatch2", "name": "Lab Rat" },
+          { "photo": "hatch3", "name": "The Nest" },
+          { "photo": "hatch4", "name": "The Ladder" },
+          { "photo": "hatch5", "name": "The Nest" }
+        ]
+      },
+      {
+        "lat": 33.805306388301986, 
+        "lng": -117.91176992339383,
+        "size": 0.01,
+        "color": "red",
+        "name": "Mission Escape Games",
+        "rooms": [
+          { "photo": "mission1", "name": "Operation: End of Days" },
+          { "photo": "mission2", "name": "Escape the Darkest Hour" },
+          { "photo": "mission3", "name": "Escape the Hydeout" },
+          { "photo": "mission4", "name": "Ultimate Bank Heist" },
+          { "photo": "mission5", "name": "Escape the Nemesis" }
+        ]
+      },
+      {
+        "lat": 37.8302581228411,
+        "lng": -122.27935550417024,
+        "size": 0.01,
+        "color": "red",
+        "name": "Trivium Games",
+        "rooms": [
+          { "photo": "trivium1", "name": "Ghost Patrol" },
+          { "photo": "trivium2", "name": "Ghost Patrol" },
+          { "photo": "trivium3", "name": "Ghost Patrol" }
+      ]
+      },
+      {
+        "lat": 33.6762402714731,
+        "lng": -111.9654508795286,
+        "size": 0.01,
+        "color": "red",
+        "name": "The Nemesis Club",
+        "rooms": [
+          { "photo": "nemesis1", "name": "Mogollon Monster" },
+          { "photo": "nemesis2", "name": "EVIL Robots" }
+      ]
+      },
+      {
+        "lat": 40.76314055001625, 
+        "lng": -111.89444665300861,
+        "size": 0.01,
+        "color": "red",
+        "name": "Himitsu Station",
+        "rooms": [
+          { "photo": "himitsu1", "name": "The Forgotten Markets" }
+      ]
+      },
+      {
+        "lat": 40.74196815934961, 
+        "lng": -111.90179454137224,
+        "size": 0.01,
+        "color": "red",
+        "name": "Escape on 13th",
+        "rooms": [
+          { "photo": "on13th_1", "name": "Arcadia" },
+          { "photo": "on13th_2", "name": "Species Unknown" }
+      ]
+      },
+      {
+        "lat": 45.534979434669424,
+        "lng": -122.62076148983225,
+        "size": 0.01,
+        "color": "red",
+        "name": "In Search of an Exit",
+        "rooms": [
+          { "photo": "insearch1", "name": "Lombino's Casino" },
+          { "photo": "insearch2", "name": "Operation Gryaznayavoda" }
+      ]
+      },
+      {
+        "lat": 39.52800891817756,
+        "lng": -119.83499887988916,
+        "size": 0.01,
+        "color": "red",
+        "name": "Keystone Escape Games",
+        "rooms": [
+          { "photo": "keystone1", "name": "Blood of the Original" },
+          { "photo": "keystone2", "name": "Dragon Alley" }
+      ]
+      },
+      {
+        "lat": 39.46156738372914,
+        "lng": -119.78329468670667,
+        "size": 0.01,
+        "color": "red",
+        "name": "Deadline Escape Rooms",
+        "rooms": [
+          { "photo": "deadline1", "name": "Camp Ruby Lake [Lady of the Lake]" },
+          { "photo": "deadline2", "name": "Dead Silence" },
+          { "photo": "deadline3", "name": "Witch Doctor" },
+          { "photo": "deadline4", "name": "Noir [Ep. 1]" }
+      ]
+      },
+      {
+        "lat": 47.20715775723151,
+        "lng": -122.48262601897335,
+        "size": 0.01,
+        "color": "red",
+        "name": "Cityscape Games",
+        "rooms": [
+          { "photo": "cityscape1", "name": "Zookeeper" },
+          { "photo": "cityscape2", "name": "Rockstar" }
+      ]
+      },
+      {
+        "lat": 47.65015781338505,
+        "lng": -122.35026413920069,
+        "size": 0.01,
+        "color": "red",
+        "name": "Locurio",
+        "rooms": [
+          { "photo": "locurio1", "name": "The Vanishing Act" },
+          { "photo": "locurio2", "name": "The Storykeeper" }
+      ]
+      },
+      {
+        "lat": 47.61832644226349,
+        "lng": -122.3576640132686,
+        "size": 0.01,
+        "color": "red",
+        "name": "Hourglass Escapes",
+        "rooms": [
+          { "photo": "hourglass1", "name": "Evil Dead 2" },
+          { "photo": "hourglass2", "name": "Mystery At Innsmouth" }
+      ]
+      },
+      {
+        "lat": 20.888986382844003,
+        "lng": -156.5026235638629,
+        "size": 0.01,
+        "color": "red",
+        "name": "Mystery Maui",
+        "rooms": [
+          { "photo": "mysterymaui1", "name": "The Ramen Shop" },
+          { "photo": "mysterymaui2", "name": "Stella Superstar!" }
+      ]
+      },
+]
